49e8d9d8404dd19f46458ddc51e9da8115493454
nturpin <nova.t974@gmail.com>
2018-06-07 16:51:31 +0400

    suite

--- js/script.js
+++ js/script.js
@@ -1,20 +1,21 @@
   
 $(function () {
-    function trierList (competences){
-        competences.sort(function (a, b) {
+    
+    var listComp = ['debutant', 'intermediaire', 'expert'];
+    function trierList (listComp) {
+        listComp.sort(function (a, b) {
             return b.value - a.value;
           });
     }
-
     $(document).ready(function() {
         $.ajax({
         type: 'GET',
         url: 'http://localhost:4002/competences',
         success: function (competences) {
-            
+             trierList (listComp);
             // affiche la liste des competences
             for (var i = 0; i < competences.length; i++) {
-                trierList (competences);
+                
                 var niveau = '<i class="fas fa-star"></i>';
                 
                 // ajoute les étoiles
